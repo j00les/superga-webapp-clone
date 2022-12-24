5068b02a0112c3ff3428d8b62df3976c275fc1f8
Nabiel Alif <nabiel.alif01@gmail.com>
2022-12-24 23:43:59 +0700

    detail page layout

--- client-user/src/pages/DetailPage.tsx
+++ client-user/src/pages/DetailPage.tsx
@@ -1,20 +1,21 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
-import { useState } from 'react';
-import { Dna } from 'react-loader-spinner';
-import { toRupiah } from 'helpers/helpers';
-import { useAppDispatch, useAppSelector } from 'hooks/hooks';
-import { fetchById } from 'store/actions/product';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useState } from "react";
+import { Dna } from "react-loader-spinner";
+import { toRupiah } from "helpers/helpers";
+import { useAppDispatch, useAppSelector } from "hooks/hooks";
+import { fetchById } from "store/actions/product";
 
 export const DetailPage: React.FC = () => {
   const { id } = useParams();
   const { products } = useAppSelector((state) => state);
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const size = [36, 37, 38, 39, 40, 41, 42, 43, 44];
 
   const loading = products.isLoading;
   const description = products.productById.description;
@@ -46,20 +47,20 @@ export const DetailPage: React.FC = () => {
   }, [dispatch, id]);
 
   return (
-    <div className="flex  justify-evenly m-8">
+    <div className="flex justify-evenly m-8" id="detail-page">
       <>
         {loading ? (
           <div className="bg-blue w-[50%]">{<Skeleton height={800} />}</div>
         ) : (
           <>
             <div className="flex">
-              <div className="flex flex-col bg-red-500">
+              <div className="flex flex-col">
                 {images?.map((el, i) => {
                   return (
                     <div
                       key={i + 2}
                       onClick={() => setImageUrl(el.imgUrl)}
-                      className="border bg-red-400 max-w-[7rem]"
+                      className=" bg-red-400 max-w-[7rem]"
                     >
                       <img src={el.imgUrl} />
                     </div>
@@ -72,14 +73,14 @@ export const DetailPage: React.FC = () => {
                 <img
                   alt="products"
                   src={image ? image : images[0]?.imgUrl}
-                  className="w-full hover:scale-[1.2]"
+                  className="w-full"
                 />
               </div>
             </div>
           </>
         )}
       </>
-      <section className=" w-1/2 mt-8  text-center">
+      <section className="w-1/2 mt-8  text-center">
         <div className="title mx-auto mb-4">
           <h1 className="text-2xl font-semibold uppercase">
             {loading ? (
@@ -87,7 +88,7 @@ export const DetailPage: React.FC = () => {
                 <Skeleton height={40} />
               </div>
             ) : (
-              'Superga indonesia'
+              "Superga indonesia"
             )}
           </h1>
           <h1>
@@ -106,29 +107,42 @@ export const DetailPage: React.FC = () => {
             <Skeleton height={25} />
           </div>
         ) : (
-          <div className="price my-8 border">
-            <p className="text-lg mb-4">{toRupiah(products.productById.price)}</p>
+          <div className="price my-8 ">
+            <p className="text-lg mb-4">
+              {toRupiah(products.productById.price)}
+            </p>
 
             <div>
-              <p className="text-xl">
-                Order within 01 hours 22 minutes to receive Sat 22 October - Sun 23 October
+              <p className="text-md">
+                Order within 01 hours 22 minutes to receive Sat 22 October - Sun
+                23 October
               </p>
-              <section className="flex ml-20 mt-[2rem]  border">
-                <div>Size:</div>
-                <span className="border border-solid border-red-400 w-fit">isi</span>
-                <span className="border border-solid border-red-400 w-fit">isi</span>
-                <span className="border border-solid border-red-400 w-fit">isi</span>
+              <section className="flex ml-20 mt-[2rem]   flex-col gap-2">
+                <span className="self-start">Size:</span>
+                <div className="flex gap-2">
+                  {size.map((el) => (
+                    <span className="border pointer p-1 border-solid  w-fit">
+                      {el}
+                    </span>
+                  ))}
+                </div>
               </section>
             </div>
           </div>
         )}
 
-        <div className="desc w-1/2 mx-auto text-xl">
-          <p>{loading ? <Skeleton count={5} /> : description}</p>
-        </div>
+        <div className="desc w-1/2 mx-auto">
+          <div id="button" className="flex my-8  justify-evenly gap-3 w-1/2">
+            <label className="ml-[20rem]">
+              <span className="text-md ">Quantity</span>
+              <input className="p-2" type="number" />
+            </label>
 
-        <div className="desc w-1/2 mx-auto text-xl">
-          <div id="button" className="flex my-8 flex-col justify-evenly gap-3">
+            {loading ? (
+              <Skeleton height={50} />
+            ) : (
+              <button className="btn">add to wishlist</button>
+            )}
             {loading ? (
               <Skeleton height={50} />
             ) : (
@@ -136,11 +150,11 @@ export const DetailPage: React.FC = () => {
                 Buy Now
               </button>
             )}
-
-            {loading ? <Skeleton height={50} /> : <button className="btn">add to wishlist</button>}
           </div>
         </div>
-
+        <div className="desc w-1/2 mx-auto text-md">
+          <p>{loading ? <Skeleton count={5} /> : description}</p>
+        </div>
         {!redirectLoading && (
           <div className="flex-col items-center  w-fit  mx-auto gap-3 ">
             <p className="text-sm text-center my-auto"> Redirecting...</p>
