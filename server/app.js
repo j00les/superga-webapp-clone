4f7f89a7f62dfeff5dfb6e08826cdf82deb11f6e
Nabiel Alif <v-nabiel.gerogy@btpn.com>
2023-03-10 19:34:36 +0700

    port settings

--- server/app.js
+++ server/app.js
@@ -1,19 +1,19 @@
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
-const express = require("express");
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const express = require('express');
 const app = express();
 // const PORT = process.env.PORT || 3000;
-const PORT = 3002;
-const cors = require("cors");
-const errorHandler = require("./middlewares/error");
-const router = require("./routes");
+const PORT = 3002
+const cors = require('cors');
+const errorHandler = require('./middlewares/error');
+const router = require('./routes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", router);
+app.use('/', router);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log("jalanz", PORT);
+  console.log('jalanz', PORT);
 });
